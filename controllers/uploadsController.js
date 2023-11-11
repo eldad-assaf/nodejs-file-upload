94b3cb9af8fb6a573e72ab27af06528ed776d22c
eldad-assaf <eldadux@gmail.com>
2023-11-11 18:41:08 +0200

    controllers and routes

---
+++ controllers/uploadsController.js
@@ -0,0 +1,9 @@
+const Product = require('../models/Product')
+const {StatusCodes} = require('http-status-codes')
+
+const uploadProductImage = async (req,res)=>{
+    res.send('upload product image')
+}
+
+
+module.exports = {uploadProductImage}
