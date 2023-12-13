7d970cd3c83c13ec2f07ddaf3970993b2c80b904
eldad-assaf <eldadux@gmail.com>
2023-12-13 18:54:02 +0200

    done

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,11 +1,27 @@
 const Product = require("../models/Product");
 const path = require("path");
 const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
 
 const uploadProductImage = async (req, res) => {
+  //console.log(req.files)
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No files uploaded");
+  }
 
   const productImage = req.files.image;
 
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please upload an image");
+  }
+
+  const maxSize = 1024 *1024;
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      "Please upload an image smaller then 1 mb"
+    );
+  }
   const imagePath = path.join(
     __dirname,
     "../public/uploads/" + `${productImage.name}`
